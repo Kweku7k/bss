457a89dd2a5fd822c50b5b8a31f2f636205cdfce
Kweku7k <mr.adumatta@gmail.com>
2024-06-24 12:11:00 +0000

    added db

--- bss/settings.py
+++ bss/settings.py
@@ -78,9 +78,17 @@ WSGI_APPLICATION = 'bss.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hr',
+        'USER': 'postgres',
+        'PASSWORD': 'nimda@2021',
+        'HOST': '45.222.128.210',
+        'PORT': '5432',
     }
 }
 
